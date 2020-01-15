4ef39283d6639fd24eb734da85d1ff9fa4b1daae
bradosia <bradosia@gmail.com>
2020-01-15 14:03:35 -0800

    working build

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 #include "mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    /* Hide Console in windows
+     */
+    #ifdef _WIN32
+    FreeConsole();
+    #endif
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
